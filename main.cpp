2b6361eb802e2767a15a3799408d7a061e251cd2
Jacobo Soffer <sofferjacob@gmail.com>
2021-03-17 16:04:21 +0000

    Add Record

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-#include "date_time.h"
+#include "record.h"
 
 using namespace std;
 
 int main() {
-  DateTime date1("Sep 23 12:58:18");
-  DateTime date2("Jul 09 16:19:58");
+  Record record1("Sep 23 12:58:18 80.169.79.65:1150 Failed password for illegal user root");
+  Record record2("Jul 09 16:19:58 142.248.253.242:2871 Illegal user");
 
+  DateTime date1 = record1.getDate();
+
+  cout << record1 << endl;
+  cout << record2 << endl;
   cout << date1 << endl;
-  cout << date2 << endl;
+  cout << record2 << endl;
   cout << date1.getMonth() << endl;
-  cout << date2.getMonth() << endl;
-  cout << (date1 == date2) << endl;
-  cout << (date1 != date2) << endl;
-  cout << (date1 > date2) << endl;
-  cout << (date1 < date2) << endl;
+  //cout << date2.getMonth() << endl;
+  //cout << (date1 == date2) << endl;
+  //cout << (date1 != date2) << endl;
+  cout << (record1 > record2) << endl;
+  cout << (record1 < record2) << endl;
 }
--- record.h
+++ record.h
@@ -1,7 +1,20 @@
-#include <time.h>
+#include <iostream>
+#include "date_time.h"
 
 class Record {
   public:
-    const string ip;
-    const string reason;
-}
+    Record(std::string);
+    const std::string ip;
+    const std::string port;
+    const std::string reason;
+    const std::string entry;  // The full entry
+    
+    DateTime getDate();
+
+    // For convenience
+    bool operator>(Record&);
+    bool operator<(Record&);
+    friend std::ostream& operator<<(std::ostream&, Record&);
+  private:
+    DateTime date;
+};
